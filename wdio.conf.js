4c7d851c3d76d157590517fe53987380d439c37c
Danny Andres Barrientos Agudelo <8796408+dannybarrientos@users.noreply.github.com>
2023-10-19 16:00:57 -0500

    SerenityJs With WebDriverIO with Tags a headless

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,7 +51,11 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu']
+        }
+        
     }],
 
     //
